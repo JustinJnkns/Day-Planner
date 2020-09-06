bb5d19cb617008afaf90334619848adae14d5285
JustinJnkns <69018193+JustinJnkns@users.noreply.github.com>
2020-09-06 18:47:30 -0400

    added save function to each hour of day

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,28 @@
-// Post the current day and date at the top of the page
+// displays day in header
 var currentDay = moment().format("dddd MMMM Do");
 
 var displayDay = function() {
     $("#currentDay").text(currentDay);
 };
 displayDay()
+
+//function to save text area to local storeage
+$(document).ready(function() {
+    $(".saveBtn").on('click', function() {
+        var saveDescription = $(this).siblings(".description").val();
+        var saveHour = $(this).parent().attr("id");
+  
+        localStorage.setItem(saveHour, saveDescription);
+    })
+  
+    $("#8am .description").val(localStorage.getItem("8am"));
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12pm .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
+  })
